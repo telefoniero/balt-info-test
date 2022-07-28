7b0b5c67c69243ee75212957bc02c658c9887d2b
Alex Topunov <telefoniero@github.com>
2022-07-28 21:23:27 +0300

    disable proxy on prod

--- src/api/VK/meta.js
+++ src/api/VK/meta.js
@@ -2,5 +2,5 @@ export const access_token =
   'vk1.a.RsNQP-lSUZzwGzCBEaj4tuRMTxIoSWjfckB7lLPJ6gVgzL-ZrZ11fOd72byPXIYpZTe5aSkxj7I6vCdQxTjmiPlBgaHO3Z35j874a8QyAw7iyGb0iIER2v4iR2Ou_lIOb-S3mCTwK0drzCJJZkJmcntp0FqHyJQDQkSK9m2m6-UAVEMjUO6Rbswa8R8P4aXn'
 // origin = http://localhost:8010/proxy
 // proxy = http://api.vk.com/method — watch package.json
-export const url = 'http://localhost:8010/proxy'
+export const url = process.env.NODE_ENV === 'production' ? "http://api.vk.com/method" : 'http://localhost:8010/proxy'
 export const v = 5.131
