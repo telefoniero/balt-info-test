a230f8096c01fdd3b3e0fe9bc9fe27b8774bbcfe
Alex Topunov <telefoniero@github.com>
2022-07-28 21:19:36 +0300

    trying to avoid noscript

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 
 import router from '@/global/router'
 import '@/assets/scss/main.scss'
-import isLoading from '@/composables/watch/friends'
 
 createApp(App).use(router).mount('#app')
